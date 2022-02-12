5af45865a60fb58880833e1aba6eb37cc38fc0ef
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-02-12 15:06:35 +0100

    added load css

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -22,6 +22,11 @@ module.exports = {
         ],
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /node_modules/,
+      },
     ],
   },
   plugins: [
