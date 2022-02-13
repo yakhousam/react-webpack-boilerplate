64d4a45393cb2a4311e5ffc2ca162c797d7e59c0
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-02-13 13:26:20 +0100

    target specific borwsers using pollyfill

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -15,7 +15,22 @@ module.exports = {
           {
             loader: "babel-loader",
             options: {
-              presets: ["@babel/preset-env", "@babel/preset-react"],
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    targets: [
+                      "last 2 versions",
+                      "not dead",
+                      "not < 2%",
+                      "not ie 11",
+                    ],
+                    useBuiltIns: "entry",
+                    corejs: "core-js@3.21.0",
+                  },
+                ],
+                "@babel/preset-react",
+              ],
               plugins: ["react-hot-loader/babel"],
             },
           },
