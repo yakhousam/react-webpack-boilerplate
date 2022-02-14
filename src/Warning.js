7bc63a7c3116434e8d98e1b59c9730d3ca90cd37
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-02-14 19:04:17 +0100

    fixed prop types error

--- src/Warning.js
+++ src/Warning.js
@@ -1,5 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
 
-export default function Warning() {
-  return <span className="warning">Take it easy</span>
+export default function Warning({ dep = false }) {
+  const [name, setName] = useState('')
+  useEffect(() => {
+    if (dep) {
+      setName('samir')
+    }
+  }, [dep])
+  return <span className="warning">Take it easy {name}</span>
+}
+
+Warning.propTypes = {
+  dep: PropTypes.bool,
 }
