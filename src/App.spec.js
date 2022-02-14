2705fe9ac545235548e047b0a5159dfc422eca82
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-02-14 18:54:17 +0100

    Added husky precommit hooks

--- src/App.spec.js
+++ src/App.spec.js
@@ -4,7 +4,7 @@ import { render } from '@testing-library/react'
 import App from './App'
 
 describe('App', () => {
-  it('render', () => {
+  it('render the app', () => {
     render(<App />)
   })
 })
